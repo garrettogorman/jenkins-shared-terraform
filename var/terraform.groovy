ce1cc4236d2fed9e321dba46db26459006eae668
Garrett O'Gorman <garrett.ogorman@qstream.com>
2018-12-13 14:35:05 +0000

    Added Groovy script to /var

---
+++ var/terraform.groovy
@@ -0,0 +1,30 @@
+#!/usr/bin/groovy
+
+def call(String lockId, String environment, String blueprintDir, String githubTeam) {
+    def terraform = new garrettogorman.jenkins.Terraform()
+
+    lock(lockId) {
+      dir(blueprintDir) {
+
+        terraform.setup(environment)
+
+        switch(terraform.plan(environment)) {
+          case 0:
+            echo 'No changes to make'
+            terraform.apply(environment)
+            break
+          case 1:
+            err 'An error has occured'
+            break
+          case 2:
+            timeout(time: 1, unit: 'DAYS') {
+              input message: "Apply infrastructure changes? (Click 'Proceed' to continue)"
+              echo 'Applying changes'
+              terraform.apply(environment)
+            }
+            break
+        }
+        return terraform.output()
+      }
+    }
+}
